94b9060cd50d791c01c6269b0d203d28d9f96b71
Carlos Brando <eduardobrando@gmail.com>
2012-04-16 16:05:16 -0300

    Fix indentation.

--- tests/minunit.h
+++ tests/minunit.h
@@ -15,16 +15,16 @@
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
 	argc = 1;\
     debug("----- RUNNING: %s", argv[0]);\
-        printf("----\nRUNNING: %s\n", argv[0]);\
-        char *result = name();\
-		if (result != 0) {\
-			printf("FAILED: %s\n", result);\
-		}\
-		else {\
-			printf("ALL TESTS PASSED\n");\
-		}\
+    printf("----\nRUNNING: %s\n", argv[0]);\
+    char *result = name();\
+	if (result != 0) {\
+		printf("FAILED: %s\n", result);\
+	}\
+	else {\
+		printf("ALL TESTS PASSED\n");\
+	}\
 	printf("Tests run: %d\n", tests_run);\
-			exit(result != 0);\
+	exit(result != 0);\
 }
 
 
